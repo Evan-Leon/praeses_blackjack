d84cf509155bedfb6e440c6c13d2fad4beb97531
Evan Leon <evan.leon.j@gmail.com>
2025-12-05 10:41:12 -0500

    Update allowed hosts for heroku

--- praeses_blackjack/settings.py
+++ praeses_blackjack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n6md*33cqs@av$#=c=)x_bdd-(dyuo2bc@ghum@0543u_@3q)n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['praeses-blackjack-5101a346b252.herokuapp.com']
+ALLOWED_HOSTS = ['praeses-blackjack-5101a346b252.herokuapp.com', '127.0.0.1:8000', '127.0.0.1']
 
 
 # Application definition
